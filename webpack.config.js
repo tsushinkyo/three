065f47e5d247e7ad53bddd5456b56db7b55509d7
Alejandro Chacón <alexroolz@gmail.com>
2017-05-25 14:21:05 +0200

    basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'css-loader',
+          'style-loader'
+        ]
+      },
+      {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    }
+    ]
+  }
+};
