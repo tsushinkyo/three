ea2549171748f87a86831c489baf37ce5139cf05
Alejandro Chacón <alexroolz@gmail.com>
2017-05-25 14:42:19 +0200

    react

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,21 @@
 var path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 module.exports = {
-  entry: './app/index.js',
+  entry: './client/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'css-loader',
-          'style-loader'
-        ]
-      },
-      {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
-      }
-    }
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 };
